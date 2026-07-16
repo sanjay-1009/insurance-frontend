eff2ccd009a765d3a20debf47cfbb562f8409b53
sanjay-1009 <gskgm2006@gmail.com>
2026-07-16 15:41:38 +0530

    Changed Localhost to Deployed URL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL:
-        "http://localhost:8080/InsuranceClaimSystem"
+        "https://signum-insurance-system.onrender.com"
 });
 
 API.interceptors.request.use(
